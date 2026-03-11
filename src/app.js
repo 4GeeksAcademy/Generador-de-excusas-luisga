3f4c8623bbfc04160e59911a911f398a97c0b5ca
Luis Gabriel Noriega <luisganoriega@gmail.com>
2026-03-11 18:52:03 +0000

    Finished excuse generator

--- src/app.js
+++ src/app.js
@@ -2,28 +2,29 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-
-  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'my phone', 'the car'];
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
   let when = [
-    'before the class',
-    'when I was sleeping',
-    'while I was exercising',
-    'during my lunch',
-    'while I was praying'
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
   ];
 
-  function randomElement(array){
+  function randomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
 
   let excuse =
-    randomElement(who) + " " +
-    randomElement(action) + " " +
-    randomElement(what) + " " +
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
     randomElement(when);
 
   document.querySelector("#excuse").innerHTML = excuse;
-
 };
